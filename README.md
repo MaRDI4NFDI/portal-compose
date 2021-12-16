6176228eec75146524869aa555aa627f20dd8592
aot29 <alvaro@zbmath.org>
2021-12-16 14:19:49 +0100

    Disable write access by default, Disable account creation, Adjust title, Link impressum, Set up MaRDI Logo

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ docker-composer repo for mardi
 ## Installation
 copy ./mediawiki/template.env to ./.env
 
-Change parameters in .env if required, this file will not be committed.
+Change parameters for your local installation in .env if required, this file will not be committed.
 
 ## More
 See [Discussion in the proejct's wiki](https://github.com/MaRDI4NFDI/portal-compose/wiki)
---
+++ mediawiki/LocalSettings.override.php
@@ -0,0 +1,58 @@
+<?php
+
+/*******************************/
+/* Enable Federated properties */
+/*******************************/
+#$wgWBRepoSettings['federatedPropertiesEnabled'] = true;
+
+/*******************************/
+/* Enables ConfirmEdit Captcha */
+/*******************************/
+#wfLoadExtension( 'ConfirmEdit/QuestyCaptcha' );
+#$wgCaptchaQuestions = [
+#  'What animal' => 'dog',
+#];
+
+#$wgCaptchaTriggers['edit']          = true;
+#$wgCaptchaTriggers['create']        = true;
+#$wgCaptchaTriggers['createtalk']    = true;
+#$wgCaptchaTriggers['addurl']        = true;
+#$wgCaptchaTriggers['createaccount'] = true;
+#$wgCaptchaTriggers['badlogin']      = true;
+
+/*******************************/
+/* Disable UI error-reporting  */
+/*******************************/
+#ini_set( 'display_errors', 1 );
+
+# Prevent new user registrations except by sysops
+$wgGroupPermissions['*']['createaccount'] = false;
+
+# Restrict anonymous editing
+$wgGroupPermissions['*']['edit'] = false;
+
+# Set name of the wiki
+$wgSitename = 'MaRDI portal';
+
+# Set MaRDI Logo
+$wgLogo = $wgScriptPath . '/images/MaRDI_Logo.png';
+
+# Footer
+$wgHooks['SkinTemplateOutputPageBeforeExec'][] = function( $sk, &$tpl ) {
+        # Remove existing entries
+        $tpl->set('about', FALSE);
+        $tpl->set('privacy', FALSE);
+        $tpl->set('disclaimer', FALSE);
+        return true;
+};
+$wgHooks['SkinAddFooterLinks'][] = function ( Skin $skin, string $key, array &$footerlinks ) {
+    if ( $key === 'places' ) {
+        $footerlinks['Imprint'] = Html::element( 'a',
+        [
+            'href' => 'https://www.mardi4nfdi.de/imprint',
+            'rel' => 'noreferrer noopener' // not required, but recommended for security reasons
+        ], 
+        'Imprint');
+    };
+    return true;
+};
