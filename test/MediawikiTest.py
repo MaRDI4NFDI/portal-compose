97cce9b1de1e0b8adb6d0aa3a2aee56bfe38937f
aot29 <alvaro@zbmath.org>
2021-12-20 12:14:43 +0100

    Added test for Math search

--- test/MediawikiTest.py
+++ test/MediawikiTest.py
@@ -1,10 +1,34 @@
-"""A simple test class example."""
+"""Test core MediaWiki installation."""
 from dockerSelenium.Base import Base
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+import os
 
-class MediawikiCoreTest(Base):
+
+class MediawikiBase(Base):
+    def _login(self):
+        """
+            Logins to the Wiki using the web UI.
+            @raise TimeoutException when not successful.
+        """
+        login_url = "http://mardi-wikibase/wiki/Special:UserLogin"
+        self.loadURL(login_url)
+        self.getElementById('wpName1').clear()
+        self.getElementById('wpName1').send_keys("Admin")
+        self.getElementById('wpPassword1').clear()
+        # get password from environment if given (i.e. in CI)
+        if 'MW_ADMIN_PASS' in os.environ.keys():
+            password = os.environ['MW_ADMIN_PASS']
+        else:
+            password = "change-this-password"
+        self.getElementById('wpPassword1').send_keys(password) 
+        self.getElementById('wpLoginAttempt').submit()
+        # wait for login to redirect to another page (i.e. Home Page)
+        WebDriverWait(self.driver, 5).until(EC.url_changes(login_url))
+
+
+class MediawikiCoreTest(MediawikiBase):
     """
     Test for Mediawiki core.
 
@@ -59,3 +83,4 @@ class MediawikiCoreTest(Base):
         self.assertTrue(
             'You do not have permission to edit this page' in page_text, 
             "Anonymous editing has not been disabled")
+
