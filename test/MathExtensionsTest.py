c1c34ba0ee478ed7c391b069eb39db72a17f386f
aot29 <alvaro@zbmath.org>
2021-12-20 12:55:51 +0100

    Removed too complex tests for now

--- test/MathExtensionsTest.py
+++ test/MathExtensionsTest.py
@@ -3,7 +3,6 @@ from MediawikiTest import MediawikiBase
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 class MathExtensionsTest(MediawikiBase):
     """Test that Math extensions are properly installed."""
     
@@ -14,21 +13,19 @@ class MathExtensionsTest(MediawikiBase):
         element = self.getElementById("bodyContent")
         self.assertTrue('Render mathematical formulas' in element.text, "Extension Math not installed.")
         self.assertTrue('MathSearch' in element.text, "Extension Math not installed.")
-        
-    def test2(self):
-        """Test that a Math search input form can be created."""
-        self._login()
-        # create or edit page Math_search_test
-        test_page_url = "http://mardi-wikibase/wiki/Math_search_test?action=edit"
-        self.loadURL(test_page_url)
-        self.getElementById('wpTextbox1').clear() # clear the page
-        self.getElementById('wpTextbox1').send_keys("{{search box}}")
-        self.getElementById('wpTextbox1').submit()
-        # wait for page to display
-        WebDriverWait(self.driver, 10).until(EC.url_changes(test_page_url))
-        # Check that input field is present
-        elements = self.driver.find_elements_by_class_name("searchboxInput")
-        self.assertTrue(len(elements) == 1, "No math search input field found.")
-        
-
-
+     
+    # This works only if the templates from mediawiki/extras are present
+    #def test2(self):
+    #    """Test that a Math search input form can be created."""
+    #    self._login()
+    #    # create or edit page Math_search_test
+    #    test_page_url = "http://mardi-wikibase/wiki/Math_search_test?action=edit"
+    #    self.loadURL(test_page_url)
+    #    self.getElementById('wpTextbox1').clear() # clear the page
+    #    self.getElementById('wpTextbox1').send_keys("{{search box}}")
+    #    self.getElementById('wpTextbox1').submit()
+    #    # wait for page to display
+    #    WebDriverWait(self.driver, 10).until(EC.url_changes(test_page_url))
+    #    # Check that input field is present
+    #    elements = self.driver.find_elements_by_class_name("searchboxInput")
+    #    self.assertTrue(len(elements) == 1, "No math search input field found.")
