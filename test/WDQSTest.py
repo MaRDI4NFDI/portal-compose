a20eed46f36533224c1542066a7a028b4a05ea03
aot29 <alvaro@zbmath.org>
2021-12-20 18:08:51 +0100

    Renamed Quickstatements container to mardi-quickstatements (Docker internal name), added test

--- test/WDQSTest.py
+++ test/WDQSTest.py
@@ -1,7 +1,5 @@
 """Test Math extensions installation."""
 from MediawikiTest import MediawikiBase
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 class WDQSTest(MediawikiBase):
     """Test that Wikidata query service properly installed."""
@@ -10,3 +8,8 @@ class WDQSTest(MediawikiBase):
         """Test that WDQS frontend is running."""
         status = self.getUrlStatusCode("http://mardi-wdqs-frontend")
         self.assertEquals(200, status, "Problem loading wdqs frontend.")
+
+    def test2(self):
+        """Test that Quickstatements is running."""
+        status = self.getUrlStatusCode("http://mardi-quickstatements")
+        self.assertEquals(200, status, "Problem loading quickstatements.")
