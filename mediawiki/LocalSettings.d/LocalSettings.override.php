ea61399a920ed99abbf0d1a42b195ae27a6c36f1
Moritz Schubotz <github@ckurs.de>
2022-01-04 13:40:21 +0100

    Use local latexml instance
    Close #22

--- mediawiki/LocalSettings.d/LocalSettings.override.php
+++ mediawiki/LocalSettings.d/LocalSettings.override.php
@@ -69,8 +69,7 @@ wfLoadExtension( 'MathSearch' );
 $wgWBRepoSettings['formatterUrlProperty']='P10';
 $wgMathDisableTexFilter = 'always';
 
-# Temporary workaround for missing DLMF macos on wuppertal account
-$wgMathLaTeXMLUrl = 'https://drmf-latexml.wmflabs.org/convert/';
+$wgMathLaTeXMLUrl = 'http://latexml:8080/convert/';
 #overwrite settings
 $wgMathDefaultLaTeXMLSetting = array(
         'format' => 'xhtml',
