02c05aaadd0d54276191bb633d163f49b511ed2a
aot29 <alvaro@zbmath.org>
2022-01-06 15:38:57 +0100

    Added tests for backup

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/bash
 
+# run tests that hit the GUI using Selenium
 docker exec mardi-selenium bash ./start_test_runner.sh test/
+
+# Run backup tests
+docker exec -ti portal-compose_backup_1 bash /test/test_backup.sh
---
+++ test/test_backup.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+# Tests the backup shell script
+# Run this test from within the backup container
+
+# count *.gz files in /data dir
+_count_backup_files() {
+    shopt -s nullglob # make sure the next line return 0 if no files match
+    bfiles=(/data/*.gz) # only count *.gz files
+    echo ${#bfiles[@]}
+}
+
+
+test_1() {
+    printf "Test that backups are stored in the backup dir\n"
+    file_count_before=$(_count_backup_files)
+    /app/wrapper.sh
+    file_count_after=$(_count_backup_files)
+    
+    # note the spacing after [[ and around ==
+    if [[ $(($file_count_before+2)) == $file_count_after ]]; then
+        printf ' - Test backup OK: Two backup files where created.\n'
+    else
+        printf ' - Test backup FAILED: no backup where files created.\n'
+    fi
+}
+
+test_1
