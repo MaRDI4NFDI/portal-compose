9d6a271db5ac60bdc80926ea71c49934702647f0
aot29 <alvaro@zbmath.org>
2022-01-20 10:50:21 +0100

    Exposed Prometheus locally on port 9090, added test

--- test/MonitoringTest.py
+++ test/MonitoringTest.py
@@ -5,6 +5,10 @@ class GrafanaTest(MediawikiBase):
     def test1(self):
         """Test that Grafana is running."""
         status = self.getUrlStatusCode("http://portal-compose_grafana_1:3000")
-        self.assertEquals(200, status, "Problem loading home page.")
+        self.assertEquals(200, status, "Problem loading Grafana start page.")
 
+    def test2(self):
+        """Test that Prometheus is running."""
+        status = self.getUrlStatusCode("http://portal-compose_prometheus_1:9090")
+        self.assertEquals(200, status, "Problem loading Prometheus start page.")
 
