3114cd481cbe9a71a7fdb8fef4d77fe3ba1e731a
aot29 <alvaro@zbmath.org>
2022-01-24 13:21:48 +0100

    Improved failed test detection in test runner script

--- run_tests.sh
+++ run_tests.sh
@@ -2,10 +2,9 @@
 
 printf "\nTests that hit the GUI using Selenium"
 printf "\n----------------------------------------\n"
-docker exec mardi-selenium bash ./start_test_runner.sh test/
-
-printf "\nTest the backup functions"
-printf "\n----------------------------------------\n"
+docker exec mardi-selenium bash ./start_test_runner.sh test &&\
+printf "\nTest the backup functions" &&\
+printf "\n----------------------------------------\n" &&\
 docker exec portal-compose_backup_1 bash /test/test_backup.sh
 
 printf "\n"
