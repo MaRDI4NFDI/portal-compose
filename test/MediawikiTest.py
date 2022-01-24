6255db066a0717fa0b0aa62de0ce8965cc69968a
aot29 <alvaro@zbmath.org>
2022-01-24 13:18:43 +0100

    Removed test for changed password, as not useful considering the way the code is currently deployed on the server

--- test/MediawikiTest.py
+++ test/MediawikiTest.py
@@ -47,34 +47,6 @@ class MediawikiCoreTest(MediawikiBase):
         status = self.getUrlStatusCode("http://mardi-wikibase")
         self.assertEquals(200, status, "Problem loading home page.")
 
-    def test2(self):
-        """Test that Mediawiki admin password has been changed."""
-        try:
-            # open login page
-            login_url = "http://mardi-wikibase/wiki/Special:UserLogin"
-            status = self.getUrlStatusCode(login_url)
-            self.assertEquals(200, status, "Problem loading login page.")
-            self.loadURL(login_url)
-            current_url = self.driver.current_url
-            # attemp login with user Admin and default password
-            self.getElementById('wpName1').clear()
-            self.getElementById('wpName1').send_keys("Admin")
-            self.getElementById('wpPassword1').clear()
-            self.getElementById('wpPassword1').send_keys("change-this-password") 
-            self.getElementById('wpLoginAttempt').submit()
-            # wait for login to redirect to another page (i.e. Home Page)
-            WebDriverWait(self.driver, 5).until(EC.url_changes(current_url))
-            # URL changed, so login with default password succeeded,
-            # so test is BAD
-            self.assertTrue(False, "Password was not changed")
-            
-        except TimeoutException:
-            # Login reloaded the login page, 
-            # the webdriver threw a timeout exception waiting for the URL to change,
-            # login with default password did not succeed,
-            # so test is GOOD
-            self.assertTrue(True)
-
     def test3(self):
         """Test that anonymous editing is disabled."""
         edit_url = 'http://mardi-wikibase/wiki/Main_Page?action=edit'
