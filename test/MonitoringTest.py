32f956a4536affda8e547ce4c70f732d9a38fcf1
dajuno <dajuno@riseup.net>
2022-03-17 18:41:14 +0100

    fix host names for prometheus/grafana tests; use machine independent network aliases

--- test/MonitoringTest.py
+++ test/MonitoringTest.py
@@ -1,14 +1,14 @@
 """Test that monitoring tools are working"""
 from MediawikiTest import MediawikiBase
 
+
 class GrafanaTest(MediawikiBase):
     def test1(self):
         """Test that Grafana is running."""
-        status = self.getUrlStatusCode("http://portal-compose_grafana_1:3000")
+        status = self.getUrlStatusCode("http://grafana:3000")
         self.assertEquals(200, status, "Problem loading Grafana start page.")
 
     def test2(self):
         """Test that Prometheus is running."""
-        status = self.getUrlStatusCode("http://portal-compose_prometheus_1:9090")
+        status = self.getUrlStatusCode("http://prometheus:9090")
         self.assertEquals(200, status, "Problem loading Prometheus start page.")
-
