6c2a99ab1944782dc2be4459e47c856986d983a2
Eloi Ferrer <eloiferrer@protonmail.com>
2022-04-12 14:59:48 +0000

    Update absolute route to properly load Parsoid Extension

--- mediawiki/LocalSettings.d/VisualEditor.php
+++ mediawiki/LocalSettings.d/VisualEditor.php
@@ -3,7 +3,7 @@
 // https://www.mediawiki.org/wiki/Extension:VisualEditor
 ## VisualEditor Extension
 wfLoadExtension( 'VisualEditor' );
-wfLoadExtension( 'Parsoid', 'vendor/wikimedia/parsoid/extension.json' );
+wfLoadExtension( 'Parsoid', "{$IP}/vendor/wikimedia/parsoid/extension.json" );
 
 $wgVirtualRestConfig['modules']['parsoid'] = array(
     'url' => 'http://localhost' . $wgScriptPath . '/rest.php',
