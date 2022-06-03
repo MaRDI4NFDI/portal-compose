c87ff962ae1ea70e217a959c7d38b78ab478f25d
dajuno <nolte@zib.de>
2022-06-03 15:48:08 +0000

    allow SVG files and enable SVG-PNG converter librsvg

--- mediawiki/LocalSettings.d/LocalSettings.override.php
+++ mediawiki/LocalSettings.d/LocalSettings.override.php
@@ -60,12 +60,11 @@ $wgHooks['SkinAddFooterLinks'][] = function ( Skin $skin, string $key, array &$f
 };
 
 # Enabling uploads for images.
-$wgEnableUploads = true; 
-# Explicitly mentioning the file-extensions for image-upload. 
+$wgEnableUploads = true;
+# Explicitly mentioning the file-extensions for image-upload.
 $wgFileExtensions = [ 'png', 'gif', 'jpg', 'jpeg', 'webp', 'svg' ];
 # Explicitly mentioning the upload-path for image-upload.
 $wgUploadPath = $wgScriptPath . '/images/';
-$wgUploadDirectory = $wgScriptPath . '/images/';
 # Enable SVG converter
 $wgSVGConverter = 'rsvg';
 
