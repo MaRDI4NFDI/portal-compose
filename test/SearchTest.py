52f2cca91dbc250be4a85f287e7e7d0beace9566
Eloi Ferrer <eloiferrer@protonmail.com>
2022-08-11 09:41:39 +0000

    Modify test to detect Vue components

--- test/SearchTest.py
+++ test/SearchTest.py
@@ -9,6 +9,8 @@ from MediawikiTest import MediawikiBase
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import time
 
 class SearchTest(MediawikiBase):
     def test_01(self):
@@ -16,10 +18,19 @@ class SearchTest(MediawikiBase):
         home_url = "http://mardi-wikibase"
         self.loadURL(home_url)
         current_url = self.driver.current_url
-        self.getElementById('searchInput').clear()
-        self.getElementById('searchInput').send_keys("boe")
-        self.getElementById('searchInput').send_keys(Keys.ENTER)
+
+        vue = self.driver.find_elements(By.XPATH,"//div[@id='p-search']/a")
+        if vue:
+            vue[0].click()
+            time.sleep(2)
+            search_field = self.getElementByXPath("//div[@id='p-search']/descendant::input[1]")
+        else:
+            search_field = self.getElementById('searchInput')
+
+        search_field.clear()
+        search_field.send_keys("boe")
+        search_field.send_keys(Keys.ENTER)
         # wait for search to load
         WebDriverWait(self.driver, 5).until(EC.url_changes(current_url))
         search_results = self.getElementById('mw-content-text').text
-        self.assertFalse('An error has occurred while searching' in search_results, 'Error while searching')
+        self.assertFalse('An error has occurred while searching' in search_results, 'Error while searching')        
