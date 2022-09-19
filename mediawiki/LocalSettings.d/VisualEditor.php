f09f61049c12a636fc5390122b870cc40a82fe29
Johannes Stegmüller <Johannesst@gmx.de>
2022-09-19 16:59:08 +0200

    Deleting Parsoid Extension explicit

--- mediawiki/LocalSettings.d/VisualEditor.php
+++ mediawiki/LocalSettings.d/VisualEditor.php
@@ -3,8 +3,3 @@
 // https://www.mediawiki.org/wiki/Extension:VisualEditor
 ## VisualEditor Extension
 wfLoadExtension( 'VisualEditor' );
-wfLoadExtension( 'Parsoid', "{$IP}/vendor/wikimedia/parsoid/extension.json" );
-
-$wgVirtualRestConfig['modules']['parsoid'] = array(
-    'url' => 'http://localhost' . $wgScriptPath . '/rest.php',
-);
