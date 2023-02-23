a2c463c222f75f35941c5cfefe1dc653a1a3d8e2
Moritz Schubotz <github@ckurs.de>
2023-02-23 19:51:53 +0100

    Add some extensions to swMATH

--- mediawiki/LocalSettings.d/swmath.php
+++ mediawiki/LocalSettings.d/swmath.php
@@ -2,4 +2,7 @@
 if ( str_contains($_SERVER['SERVER_NAME'],'swmath') ){
   $wgDBname = 'wiki_swmath';
   $wgLogos = false;
+  # Load swMATH specifi extensions
+  wfLoadExtension( 'ExternalData' );
+  wfLoadExtension( 'UrlGetParameters' );
 }
