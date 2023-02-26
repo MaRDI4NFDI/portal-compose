f2b562976bf84379203714d5a5e581b6cde73ec5
Moritz Schubotz <github@ckurs.de>
2023-02-26 12:13:17 +0100

    Workaround for compatibility of extensions
    It was attempted to load External Data twice, from /var/www/html/extensions/ExternalData/extension.json and /var/www/html/extensions/UrlGetParameters/extension.json.
    
    Occurred when loading via
    ```
     wfLoadExtension( 'ExternalData' );
     wfLoadExtension( 'UrlGetParameters' );
    ```

--- mediawiki/LocalSettings.d/swmath.php
+++ mediawiki/LocalSettings.d/swmath.php
@@ -3,6 +3,8 @@ if ( str_contains($_SERVER['SERVER_NAME'],'swmath') ){
   $wgDBname = 'wiki_swmath';
   $wgLogos = false;
   # Load swMATH specifi extensions
-  wfLoadExtension( 'ExternalData' );
+  # wfLoadExtension( 'ExternalData' );
+  # The extension does not allow loading ExtneralData explicitly
+  # It was attempted to load External Data twice, from /var/www/html/extensions/ExternalData/extension.json and /var/www/html/extensions/UrlGetParameters/extension.json.
   wfLoadExtension( 'UrlGetParameters' );
 }
