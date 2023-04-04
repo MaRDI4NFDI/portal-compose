6ba2cccc42b8562983e2e1611a2666ec93351a49
Eloi Ferrer <eloiferrer@protonmail.com>
2023-04-04 11:53:27 +0000

    Load UrlGetParameters

--- mediawiki/LocalSettings.d/swmath.php
+++ mediawiki/LocalSettings.d/swmath.php
@@ -2,9 +2,6 @@
 if ( str_contains($_SERVER['SERVER_NAME'],'swmath') ){
   $wgDBname = 'wiki_swmath';
   $wgLogos = false;
-  # Load swMATH specifi extensions
+  # Load swMATH specific extensions
   # wfLoadExtension( 'ExternalData' );
-  # The extension does not allow loading ExtneralData explicitly
-  # It was attempted to load External Data twice, from /var/www/html/extensions/ExternalData/extension.json and /var/www/html/extensions/UrlGetParameters/extension.json.
-  wfLoadExtension( 'UrlGetParameters' );
 }
