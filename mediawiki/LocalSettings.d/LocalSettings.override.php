905393310d32f02e65e6aa0bebeb1b17c53b8142
Eloi Ferrer <eloiferrer@protonmail.com>
2023-04-04 07:09:29 +0000

    Add LinkedWiki configuration

--- mediawiki/LocalSettings.d/LocalSettings.override.php
+++ mediawiki/LocalSettings.d/LocalSettings.override.php
@@ -168,3 +168,15 @@ $wgWBRepoSettings['string-limits'] = [
         'length' => 1000,
     ],
 ];
+
+# Linked-Wiki Configuration
+$wgLinkedWikiConfigSPARQLServices["http://wikibase.svc"] = array(
+    "debug" => false,
+    "isReadOnly" => true,
+    "endpointRead" => "http://wdqs.svc:9999/bigdata/namespace/wdq/sparql",
+    "typeRDFDatabase" => "blazegraph",
+    "HTTPMethodForRead" => "GET",
+    "lang" => "en"
+);
+
+$wgLinkedWikiSPARQLServiceByDefault= "http://wikibase.svc";
