3ad090b8379ce551492b7f4c8ce554a9d966b8fc
Eloi Ferrer <eloiferrer@protonmail.com>
2023-04-04 07:18:38 +0000

    rearrange LinkedWiki configuration

--- mediawiki/LocalSettings.d/LinkedWiki.php
+++ mediawiki/LocalSettings.d/LinkedWiki.php
@@ -1,2 +1,14 @@
 <?php
 wfLoadExtension( 'LinkedWiki' );
+
+# Linked-Wiki Configuration
+$wgLinkedWikiConfigSPARQLServices["http://wikibase.svc"] = array(
+    "debug" => false,
+    "isReadOnly" => true,
+    "endpointRead" => "http://wdqs.svc:9999/bigdata/namespace/wdq/sparql",
+    "typeRDFDatabase" => "blazegraph",
+    "HTTPMethodForRead" => "GET",
+    "lang" => "en"
+);
+
+$wgLinkedWikiSPARQLServiceByDefault= "http://wikibase.svc";
--- mediawiki/LocalSettings.d/LocalSettings.override.php
+++ mediawiki/LocalSettings.d/LocalSettings.override.php
@@ -168,15 +168,3 @@ $wgWBRepoSettings['string-limits'] = [
         'length' => 1000,
     ],
 ];
-
-# Linked-Wiki Configuration
-$wgLinkedWikiConfigSPARQLServices["http://wikibase.svc"] = array(
-    "debug" => false,
-    "isReadOnly" => true,
-    "endpointRead" => "http://wdqs.svc:9999/bigdata/namespace/wdq/sparql",
-    "typeRDFDatabase" => "blazegraph",
-    "HTTPMethodForRead" => "GET",
-    "lang" => "en"
-);
-
-$wgLinkedWikiSPARQLServiceByDefault= "http://wikibase.svc";
