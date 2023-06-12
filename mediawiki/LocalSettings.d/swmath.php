3ab0fe10a8084500e079eef29ca4176cee18a745
Moritz Schubotz <github@ckurs.de>
2023-06-13 00:23:56 +0200

    swMATH use wordmark logo variant

--- mediawiki/LocalSettings.d/swmath.php
+++ mediawiki/LocalSettings.d/swmath.php
@@ -3,10 +3,15 @@ if ( str_contains($_SERVER['HTTP_HOST'],'swmath') ){
   $wgDBname = 'wiki_swmath';
   # Set name of the wiki
   $wgSitename = 'swMATH staging';
-  # Set swMATH logo and icon (also SVG?)
+  # Set swMATH logo
   $wgLogos = [
-    'svg' => $wgScriptPath . '/images_repo/swMATH.svg',
-  ];
+    'svg' => 
+    	'wordmark' => [
+        'src' => $wgScriptPath . '/images_repo/swMATH.svg',	// path to wordmark version
+        'width' => 135,
+        'height' => 30,
+	    ]
+    ];
   # Load swMATH specific extensions
   # wfLoadExtension( 'ExternalData' );
 }
