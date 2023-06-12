eb7dabe6a6797de13115a8b78fabf2ef0d698acf
Moritz Schubotz <github@ckurs.de>
2023-06-12 16:46:47 +0200

    Update swmath.php

--- mediawiki/LocalSettings.d/swmath.php
+++ mediawiki/LocalSettings.d/swmath.php
@@ -1,7 +1,13 @@
 <?php
 if ( str_contains($_SERVER['HTTP_HOST'],'swmath') ){
   $wgDBname = 'wiki_swmath';
-  $wgLogos = false;
+  # Set name of the wiki
+  $wgSitename = 'swMATH staging';
+  # Set swMATH logo and icon (also SVG?)
+  $wgLogos = [
+    'icon' => $wgScriptPath . '/images_repo/swMATH.svg',
+    'svg' => $wgScriptPath . '/images_repo/swMATH.svg',
+  ];
   # Load swMATH specific extensions
   # wfLoadExtension( 'ExternalData' );
 }
