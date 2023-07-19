f3abbe1fc0cbcfeec5df0c0132f880b8362bbe62
Moritz Schubotz <github@ckurs.de>
2023-07-19 22:38:54 +0200

    Add more settings to connect swMATH

--- mediawiki/LocalSettings.d/LocalSettings.override.php
+++ mediawiki/LocalSettings.d/LocalSettings.override.php
@@ -153,12 +153,30 @@ $wgMathWikibasePropertyIdHasPart = "P4";
 $wgWBRepoSettings['siteLinkGroups'] = [ 'mathematics' ];
 $wgWBClientSettings['siteLinkGroups'] = [ 'mathematics' ];
 $wgWBClientSettings['siteGlobalID'] = 'mardi';
+$wgWBClientSettings['repoUrl'] = 'https://portal.mardi4nfdi.de';
+$wgWBClientSettings['repoScriptPath'] = '/w';
+$wgWBClientSettings['repoArticlePath'] = '/wiki/$1';
+$wgWBClientSettings['entitySources'] = [
+        'mardi_source' => [
+                'repoDatabase' => 'my_wiki',
+                'baseUri' => 'https://portal.mardi4nfdi.de/entity',
+                'entityNamespaces' => [
+                        'item' => 120,
+                        'property' => 122,
+                ],
+                'rdfNodeNamespacePrefix' => 'wd',
+                'rdfPredicateNamespacePrefix' => '',
+                'interwikiPrefix' => 'mardi',
+        ],
+];
+$wgWBClientSettings['itemAndPropertySourceName'] = 'mardi_source';
 // my_wiki is the MaRDI database
 $wgLocalDatabases = [ 'wiki_swmath', 'my_wiki' ];
 $wgWBRepoSettings['localClientDatabases'] = [
 	'mardi' => 'my_wiki',
 	'swmath' => 'wiki_swmath'
 ];
+
 // insert site with
 // php addSite.php --filepath=https://portal.mardi4nfdi.de/w/\$1 --pagepath=https://portal.mardi4nfdi.de/wiki/\$1 --language en --interwiki-id mardi mardi mathematics
 // php addSite.php --filepath=https://staging.swmath.org/w/\$1 --pagepath=https://staging.swmath.org/wiki/\$1 --language en --interwiki-id swmath swmath mathematics
