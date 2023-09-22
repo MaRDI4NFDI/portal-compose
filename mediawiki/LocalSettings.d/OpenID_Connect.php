e70dcb9b5e6036876d48cced8589d9481473cf46
Moritz Schubotz <github@ckurs.de>
2023-09-22 18:49:52 +0200

    Update OpenID_Connect.php
    It seems github does not support openIDconnect

--- mediawiki/LocalSettings.d/OpenID_Connect.php
+++ mediawiki/LocalSettings.d/OpenID_Connect.php
@@ -8,4 +8,12 @@ $wgPluggableAuth_Config['Login with your Google Account'] = [
         'clientsecret' => $_ENV['GOOGLE_OPENID_SECRET'],
     ]
 ];
+$wgPluggableAuth_Config['Login with your GitHub Account'] = [
+    'plugin' => 'OpenIDConnect',
+    'data' => [
+        'providerURL' => 'https://github.com',
+        'clientID' => '37d22a323b1de98272a8',
+        'clientsecret' => $_ENV['GITHUB_OPENID_SECRET'],
+    ]
+];
 $wgOpenIDConnect_MigrateUsersByEmail=true;
