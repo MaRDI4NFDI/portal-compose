a3eb4723150152b20f08f02e9d2c9fcf8303fa11
Moritz Schubotz <github@ckurs.de>
2023-09-27 17:10:56 +0200

    Configure JsonConfig and enable CodeEditor (again)

--- mediawiki/LocalSettings.d/CodeEditingExtension.php
+++ mediawiki/LocalSettings.d/CodeEditingExtension.php
@@ -1,6 +1,15 @@
 <?php
 wfLoadExtension( 'JsonConfig' );
-//wfLoadExtension( 'CodeEditor' ); requires wiki editor that is not yet availible.
+// $wgJsonConfigEnableLuaSupport = true; // required to use JsonConfig in Lua
+// Content model is 'JsonConfig.CodeMeta'
+// Model class is set to NULL to allow non-validated data
+$wgJsonConfigModels['JsonConfig.CodeMeta'] = null;
+$wgJsonConfigs['JsonConfig.CodeMeta'] = [ 
+        'namespace' => NS_CODEMETA, 
+        'nsName' => 'CodeMeta',
+        'pattern' => '/.\.json/',
+];
+wfLoadExtension( 'CodeEditor' );
 $wgDefaultUserOptions['usebetatoolbar'] = 1; // user option provided by WikiEditor extension
 wfLoadExtension( 'CodeMirror' );
 // Enables use of CodeMirror by default but still allow users to disable it
