9eb80b1f9f2dfef4eaddf0d6233349da92310d77
Moritz Schubotz <github@ckurs.de>
2023-09-27 16:12:35 +0200

    Fix: Disable CodeEditor

--- mediawiki/LocalSettings.d/CodeEditingExtension.php
+++ mediawiki/LocalSettings.d/CodeEditingExtension.php
@@ -1,6 +1,6 @@
 <?php
 wfLoadExtension( 'JsonConfig' );
-wfLoadExtension( 'CodeEditor' );
+//wfLoadExtension( 'CodeEditor' ); requires wiki editor that is not yet availible.
 $wgDefaultUserOptions['usebetatoolbar'] = 1; // user option provided by WikiEditor extension
 wfLoadExtension( 'CodeMirror' );
 // Enables use of CodeMirror by default but still allow users to disable it
