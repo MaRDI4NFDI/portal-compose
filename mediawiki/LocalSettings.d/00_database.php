58563eb1ada1b7c101e65e1a79e0cc661f4fc645
Moritz Schubotz <wiki@physikerwelt.de>
2023-11-04 07:54:00 +0100

    Disable readonly to test db writes
    #413

--- mediawiki/LocalSettings.d/00_database.php
+++ mediawiki/LocalSettings.d/00_database.php
@@ -1,3 +1,2 @@
 <?php
-$wgReadOnly = 'This wiki is currently being upgraded to a newer software version. Please check back in a couple of hours.';
 $wgDBname =  str_contains($_SERVER['HTTP_HOST']??'','swmath') ? 'wiki_swmath' : 'my_wiki';
