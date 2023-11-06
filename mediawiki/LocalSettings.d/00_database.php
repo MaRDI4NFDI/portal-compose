c24501c162ca265462f15b17ba9deb014dfa0dd7
Moritz Schubotz <wiki@physikerwelt.de>
2023-11-06 16:15:24 +0100

    Use repl db for read queries
    #420

--- mediawiki/LocalSettings.d/00_database.php
+++ mediawiki/LocalSettings.d/00_database.php
@@ -1,2 +1,28 @@
 <?php
 $wgDBname =  str_contains($_SERVER['HTTP_HOST']??'','swmath') ? 'wiki_swmath' : 'my_wiki';
+$wgLBFactoryConf = array(
+
+'class' => 'LBFactoryMulti',
+
+'sectionsByDB' => array(
+    'my_wiki' => 's1', 
+    'wiki_swmath' => 's1',
+),
+
+'sectionLoads' => array(
+    's1' => array(
+        'mysql.svc'  => 0,
+        'mysql-repl.svc'  => 50, /* the 50 is the weight (of replica servers). Would matter if you had multiple */
+    ),
+),
+
+
+'serverTemplate' => array(
+    'dbname'      => $wgDBname,
+    'user'          => $wgDBuser,
+    'password'      => $wgDBpassword,
+    'type'          => 'mysql',
+    'flags'          => DBO_DEFAULT,
+    'max lag'      => 30,
+),
+);
