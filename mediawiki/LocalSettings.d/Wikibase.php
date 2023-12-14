1f0b7a3b95538bc3b788352ab1045a1027c76a18
Moritz Schubotz <wiki@physikerwelt.de>
2023-12-15 00:47:56 +0100

    Exclude items from beeing searched by default
    Attempt 2.
    
    Properties will still be searched by default
    https://github.com/MaRDI4NFDI/portal-compose/issues/224

--- mediawiki/LocalSettings.d/Wikibase.php
+++ mediawiki/LocalSettings.d/Wikibase.php
@@ -42,8 +42,13 @@ $wgNamespacesToBeSearchedDefault[122] = true; // WB_PROPERTY_NAMESPACE===122
 if ( $wgDBname !== 'wiki_swmath' ){
 
 	wfLoadExtension( 'WikibaseRepository', "$IP/extensions/Wikibase/extension-repo.json" );
-	require_once "$IP/extensions/Wikibase/repo/ExampleSettings.php";
-
+	// from https://github.com/wikimedia/mediawiki-extensions-Wikibase/blob/f2bd35609b6bf3f8d38ef8c78d2f340497906706/repo/includes/RepoHooks.php#L170C1-L180C61
+	$wgExtraNamespaces[120] = 'Item';
+	$wgExtraNamespaces[121] = 'Item_talk';
+	$wgExtraNamespaces[122] = 'Property';
+	$wgExtraNamespaces[123] = 'Property_talk';
+	// do not declare namespaces if that would be done by default https://gerrit.wikimedia.org/r/c/mediawiki/extensions/Wikibase/+/933906 https://phabricator.wikimedia.org/T291617
+	$wgWBRepoSettings['defaultEntityNamespaces'] = false;
 	$wgWBRepoSettings['localClientDatabases'] = [
 		'mardi' => 'my_wiki',
 		'swmath' => 'wiki_swmath'
