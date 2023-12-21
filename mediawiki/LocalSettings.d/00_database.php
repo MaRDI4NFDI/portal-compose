e6ce697bb8478e2309dd01b9c71bedd74bd62db6
Moritz Schubotz <wiki@physikerwelt.de>
2023-12-21 11:54:49 +0100

    Make portal readonly

--- mediawiki/LocalSettings.d/00_database.php
+++ mediawiki/LocalSettings.d/00_database.php
@@ -1,4 +1,5 @@
 <?php
+$wgReadOnly = 'This wiki is currently being upgraded to a newer software version. Please check back in a couple of hours.';
 $wgDBname =  str_contains($_SERVER['HTTP_HOST']??'','swmath') ? 'wiki_swmath' : 'my_wiki';
 if ( false && getenv('CI') !== 'true' ) {
     $wgLBFactoryConf = array(
