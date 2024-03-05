50c36ebbeeb606b9897d497560ff87c57e9e8cd5
Moritz Schubotz <wiki@physikerwelt.de>
2024-03-05 19:59:37 +0100

    Add namespace for software
    cf #450

--- mediawiki/LocalSettings.d/mardi_namespaces.php
+++ mediawiki/LocalSettings.d/mardi_namespaces.php
@@ -8,6 +8,8 @@ define("NS_CODEMETA", 4204);
 define("NS_CODEMETA_TALK", 4205);
 define("NS_PUBLICATION", 4206);
 define("NS_PUBLICATION_TALK", 4207);
+define("NS_SOFTWARE", 4208);
+define("NS_SOFTWARE_TALK", 4209);
 
 // Add namespaces.
 $wgExtraNamespaces[NS_FORMULA] = "Formula";
@@ -16,10 +18,13 @@ $wgExtraNamespaces[NS_PERSON] = "Person";
 $wgExtraNamespaces[NS_PERSON_TALK] = "Person_talk";
 $wgExtraNamespaces[NS_PUBLICATION] = "Publication";
 $wgExtraNamespaces[NS_PUBLICATION_TALK] = "Publication_talk";
+$wgExtraNamespaces[NS_SOFTWARE] = "Software";
+$wgExtraNamespaces[NS_SOFTWARE_TALK] = "Software_talk";
 
 $wgNamespaceProtection[NS_FORMULA] = array( 'overwriteprofilepages' ); 
 $wgNamespaceProtection[NS_PERSON] = array( 'overwriteprofilepages' );
-$wgNamespaceProtection[NS_PUBLICATION] = array( 'overwriteprofilepages' ); 
+$wgNamespaceProtection[NS_PUBLICATION] = array( 'overwriteprofilepages' );
+$wgNamespaceProtection[NS_SOFTWARE] = array( 'overwriteprofilepages' ); 
 
 
 $wgGroupPermissions['sysop']['overwriteprofilepages'] = true;
@@ -27,9 +32,13 @@ $wgGroupPermissions['sysop']['overwriteprofilepages'] = true;
 $wgContentNamespaces[] = NS_FORMULA;
 $wgContentNamespaces[] = NS_PERSON;
 $wgContentNamespaces[] = NS_PUBLICATION;
+$wgContentNamespaces[] = NS_SOFTWARE;
+
 
 
 $wgNamespacesToBeSearchedDefault[NS_FORMULA] = true;
 $wgNamespacesToBeSearchedDefault[NS_PERSON] = true;
 $wgNamespacesToBeSearchedDefault[NS_PUBLICATION] = true;
+$wgNamespacesToBeSearchedDefault[NS_SOFTWARE] = true;
+
 
