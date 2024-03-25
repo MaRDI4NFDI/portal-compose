8067b2371586989a13a5f0ca1cbaf9a5e5d9100a
Eloi Ferrer <eloiferrer@protonmail.com>
2024-03-25 11:24:12 +0000

    define MSC classification sql query

---
+++ mediawiki/LocalSettings.d/MSC_classification.php
@@ -0,0 +1,14 @@
+<?php
+$wgExternalDataSources['MSC'] = [
+    'server' => getenv('DB_SERVER'),
+    'type' => 'mysql',
+    'name' => 'my_wiki',
+    'user' => getenv('DB_USER'),
+    'password' => getenv('DB_PASS'),
+    'prepared' => <<<'SQL'
+SELECT msc_string
+FROM msc_id_mapping
+WHERE msc_id = ?
+SQL,
+	  'types' => 's'
+];
