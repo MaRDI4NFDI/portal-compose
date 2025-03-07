5994e3fe3e5598ec8d9c3f20de5e2f94f96e9e71
Moritz Schubotz <wiki@physikerwelt.de>
2025-03-07 22:58:37 +0100

    Update WikibaseFacetedSearch.php
    Test fix for https://github.com/ProfessionalWiki/WikibaseFacetedSearch/issues/223

--- mediawiki/LocalSettings.d/WikibaseFacetedSearch.php
+++ mediawiki/LocalSettings.d/WikibaseFacetedSearch.php
@@ -1,3 +1,2 @@
 <?php
-# temporary disabled per https://github.com/ProfessionalWiki/WikibaseFacetedSearch/issues/223
-# wfLoadExtension( 'WikibaseFacetedSearch' );
+wfLoadExtension( 'WikibaseFacetedSearch' );
