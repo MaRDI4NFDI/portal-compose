48414fd319a987b40f8b95684da1b74c8538dd61
Moritz Schubotz <wiki@physikerwelt.de>
2025-03-21 16:52:40 +0100

    Use display_title for search ranking
    Instead of using the regular title, use the displaytitle for ranking.

--- mediawiki/LocalSettings.d/DisplayTitle.php
+++ mediawiki/LocalSettings.d/DisplayTitle.php
@@ -5,3 +5,14 @@ $wgRestrictDisplayTitle=false;
 # The Display Title extension allows a page's display title to be used as the default link text in links to the page - both links from other pages as well as self-links on the page.
 # https://www.mediawiki.org/wiki/Extension:Display_Title
 wfLoadExtension( 'DisplayTitle' );
+// Replace display_title with title fields otherwise keep the defaults from https://gerrit.wikimedia.org/g/mediawiki/extensions/CirrusSearch/%2B/HEAD/docs/settings.txt
+$wgCirrusSearchWeights = [
+    'display_title' => 20,
+    'redirect' => 15,
+    'category' => 8,
+    'heading' => 5,
+    'opening_text' => 3,
+    'text' => 1,
+    'auxiliary_text' => 0.5,
+    'file_text' => 0.5,
+];
