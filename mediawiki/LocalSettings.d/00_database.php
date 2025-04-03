b7e3f34dbd2badfef5d951112310a582c80bf893
physikerwelt <wiki@physikerwelt.de>
2025-04-03 10:43:29 +0200

    Use respective database for wikimedia wikis
    Set appropriate language.

--- mediawiki/LocalSettings.d/00_database.php
+++ mediawiki/LocalSettings.d/00_database.php
@@ -3,16 +3,31 @@
 use MediaWiki\Logger\LoggerFactory;
 
 $wgDBname = 'my_wiki';
+$host = $_SERVER['HTTP_HOST'] ?? false;
 if ( defined( 'MW_DB' ) ) {
 	// Set $wikiId from the defined constant 'MW_DB' that is set by maintenance scripts.
 	$wgDBname = MW_DB;
-} elseif ( !isset( $_SERVER['HTTP_HOST'] ) ) {
+} elseif ( $host === false ) {
 	$logger = LoggerFactory::getInstance( 'MaRDIconf' );
 	$logger->warning( 'Server name not set. Falling back to my_wiki.' );
-} elseif ( str_contains( $_SERVER['HTTP_HOST'], 'swmath' ) ) {
+} elseif ( str_contains( $host, 'swmath' ) ) {
 	$wgDBname = 'wiki_swmath';
-} elseif ( str_contains( $_SERVER['HTTP_HOST'], 'en.wiki' ) ) {
-	$wgDBname = 'enwiki';
+} elseif ( str_contains( $host, '.wik' ) ) {
+	$wikibase_host = getenv( 'WIKIBASE_HOST' );
+	if ( preg_match( '/^([0-9a-z-]+)\.(wik.*?)' . $wikibase_host . '$/', $host, $match ) !== 1 ) {
+		die( "Server name $host does not match the patterns for wikis." );
+	}
+	$lang = str_replace( '-', '_', $match[1] );
+	$wgDBname = $lang . $match[2];
+}
+
+/** Set language code */
+if ( preg_match( '/^([a-z_]+)(wik.*?)$/', $wgDBname, $match ) !== 1 ) {
+	$lang = str_replace( '_', '-', $match[1] );
+	if ( LanguageCode::isWellFormedLanguageTag( $lang ) ) {
+		$wgLanguageCode = $lang;
+	}
+	// fall back to english
 }
 
 if ( false && getenv( 'CI' ) !== 'true' ) {
