8af1d3480bde2f8c682536ec657ea8bd6080e71e
Larissa Pusch <larissapusch@gmx.net>
2025-06-02 16:01:49 +0200

    give namespace permissions to all

--- mediawiki/LocalSettings.d/mardi_namespaces.php
+++ mediawiki/LocalSettings.d/mardi_namespaces.php
@@ -82,7 +82,7 @@ $wgNamespaceProtection[NS_QUANTITY] = array( 'overwriteprofilepages' );
 $wgNamespaceProtection[NS_TASK] = array( 'overwriteprofilepages' );
 
 
-$wgGroupPermissions['sysop']['overwriteprofilepages'] = true;
+$wgGroupPermissions['*']['overwriteprofilepages'] = true;
 
 $wgContentNamespaces[] = NS_FORMULA;
 $wgContentNamespaces[] = NS_PERSON;
