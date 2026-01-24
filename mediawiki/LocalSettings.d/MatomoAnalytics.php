d1ad5e650d73355324f4ec84cbc5d874b6d5a561
physikerwelt <wiki@physikerwelt.de>
2026-01-25 00:01:24 +0100

    Disable MatomoAnalytics extension loading
    Currently, the extension seems dysfunctional.
    Multiple errors per second
    
    ```
    [2026-01-24T22:58:44.402689+00:00] error.ERROR: [{reqId}] {exception_url}   PHP Warning: foreach() argument must be of type array|object, null given {"exception":"[object] (ErrorException(code: 0): PHP Warning: foreach() argument must be of type array|object, null given at /var/www/html/w/extensions/MatomoAnalytics/includes/MatomoAnalyticsWiki.php:59)","exception_url":"/w/index.php?action=info&title=Publication:4104377","reqId":"60bf7c88f655014ba029b4a1","caught_by":"mwe_handler"} []
    ``

--- mediawiki/LocalSettings.d/MatomoAnalytics.php
+++ mediawiki/LocalSettings.d/MatomoAnalytics.php
@@ -1,4 +1,4 @@
 <?php
-wfLoadExtension( 'MatomoAnalytics' );
+#wfLoadExtension( 'MatomoAnalytics' );
 $wgMatomoAnalyticsServerURL='https://matomo.' . getenv('WIKIBASE_HOST') .'/';
 $wgMatomoAnalyticsTokenAuth=$_ENV['MATOMO_TOKEN'];
